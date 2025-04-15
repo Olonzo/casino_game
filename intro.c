09dbdb12b4743bfa8c1928fff65445177794b79a
Olonzo <evanskalonzo100@gmail.com>
2025-04-15 13:34:10 +0300

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int rules();
+
+int round1();
+
+int main(){
+
+    rules();
+
+    printf("\n\n");
+
+    round1();
+
+    return 0;
+}
+
+int round1(){
+
+    char name[50];
+    printf("Enter your name : ");
+    scanf("%s",name);
+
+    printf("\n\n");
+
+    int deposit;
+    printf("Enter the amount of money to deposit for the game : ");
+    scanf("%d",&deposit);
+
+    printf("\n\n");
+
+    printf("Your current balance is %d",deposit);
+
+    int bet1;
+    printf("%s,Enter the money to bet : ", name);
+
+    return 0;
+}
